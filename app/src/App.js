8d4ba79392db44a990f3fb4f8d9f67e59d16cb6f
TaylorDarneille <tmdarneille@dons.usfca.edu>
2018-09-20 15:14:58 -0700

    installed react-materialize and initialized noded

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Navbar, NavItem } from 'react-materialize';
 import './App.css';
 
 class App extends Component {
@@ -6,6 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <Navbar brand='logo' right>
+            <NavItem onClick={() => console.log('test click')}>Getting started</NavItem>
+            <NavItem href='components.html'>Components</NavItem>
+          </Navbar>
         </header>
       </div>
     );
