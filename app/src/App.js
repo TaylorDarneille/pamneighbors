e9a24b2204c0188fe315a79763702bd226244b55
TaylorDarneille <tmdarneille@dons.usfca.edu>
2018-09-24 14:40:14 -0700

    removed node from root directory, added materializeUI node package to app folder, removed old materialize code, using css baseline and typography suggestions

--- app/src/App.js
+++ app/src/App.js
@@ -2,19 +2,16 @@ import React, { Component } from 'react';
 import { Navbar, NavItem } from 'react-materialize';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Navbar brand='logo' right>
-            <NavItem onClick={() => console.log('test click')}>Getting started</NavItem>
-            <NavItem href='components.html'>Components</NavItem>
-          </Navbar>
-        </header>
-      </div>
-    );
-  }
+// see docs: https://material-ui.com/style/css-baseline/
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+function App() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      {/* The rest of your application */}
+    </React.Fragment>
+  );
 }
 
 export default App;
