dc6367c64705b9cbaae1f4b2f06d2ea17db95791
TaylorDarneille <tmdarneille@dons.usfca.edu>
2018-10-17 19:34:41 -0700

    reformatted video player to fill entire banner

--- app/src/Video.js
+++ app/src/Video.js
@@ -1,29 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import ReactPlayer from 'react-player';
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
     textAlign: 'center',
-    background: 'black',
-    height: '40vh',
-    overflow: 'hidden'
+    height: '350px',
+    overflow: 'hidden',
+    position: "relative",
   },
   vid: {
-    width: '75%',
-    // height: 'inherit',
-    margin: '0 auto',
-  }
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+    transform: "translateY(-50%) translateX(-50%)"
+  },
+
 });
+
+
 
 function Video(props) {
   const { classes } = props;
 
   return (
     <div className={classes.root}>
-      <ReactPlayer className={classes.vid} url='https://www.youtube.com/watch?v=jMFuWTRtEKo' playing />
+      <iframe className={classes.vid} type="text/html" width="200%" height="200%"
+      src="https://www.youtube.com/embed/jMFuWTRtEKo?autoplay=1&modestbranding=1&mute=1&controls=0"
+      frameborder="0"/> 
     </div>
   );
 }
